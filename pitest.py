036160827d48c0153055608f508fe02651c950d0
Ic3Venom <Ic3Venom@gmail.com>
2017-02-17 23:13:24 -0800

    Support userInput of test type
    Heavy influence from stoichiometry.py's userInput IO.

--- pitest.py
+++ pitest.py
@@ -5,9 +5,33 @@ def test2():
 def test3():
     pass
 
-def main():
-  pass
 
+def main():
+    while True:
+        print 'What pi test would you like to take? (type \'help\' for help)'
+        userInput = raw_input(">>> ")
+
+        if userInput.lower() == 'help':
+            print '\nWelcome to the pitest.py help page!\n'
+            print 'For single digit reciting, type \'1\' or \'single\''
+            print 'For ten digits at a time, type \'2\' or \'ten\''
+            print 'For the all digits at once, type \'3\' or \'all\''
+            print '\nAny bugs, issues, requests? Put them in the GitHub repo.\n'
+
+        elif userInput.lower() == 'quit':
+            exit(0)
+
+        elif userInput.lower() in ['1', 'single']:
+            test1()
+            break
+
+        elif userInput.lower() in ['2', 'ten']:
+            test2()
+            break
+
+        elif userInput.lower() in ['3', 'all']
+            test3()
+            break
 if __name__ == '__main__':
     main()
 
