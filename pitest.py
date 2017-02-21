77abdcc3232e21b674bbe00fdf40535af8d7f0e1
Ic3Venom <Ic3Venom@gmail.com>
2017-02-20 20:40:03 -0800

    Nearing program completion
    Finished nearly everything needed, now need to add previous numbers when starting the continuation mode. Need to fix EOF and when user asks for an index equal to or greater than the end of the 'pi.bin' file.

--- pitest.py
+++ pitest.py
@@ -1,26 +1,57 @@
 def test( testLen ):
-    print 'Lets begin! 3. ...'
+    i = 0
+
+    try:
+        if testLen == 1:
+            userInput = int( raw_input('Enter the next digit: 3.') )
+
+        elif testLen == 2:
+            i = int( raw_input('What digit would you like to start on?:' ) )
+            testLen = 1
+            userInput = int( raw_input( 'Continue from digit %d: ' % i) )
+
+        elif testLen == 10:
+            userInput = int( raw_input( 'Enter the next ten digits: 3.' ) )
+
+    except:
+        print 'Something went wrong when obtaining user offset! Exiting program.'
+        exit(1)
 
     with open('pi.bin', 'r') as f:
-        byte = int( f.read(testLen) )
-        i    = 0
-        while byte != "":
-            if i == 1
-                userInput = int( raw_input("Enter the next digit (%d):" % (i + 3)) )
-            elif i == 10
-                userInput = int( raw_input("Enter the next ten digits (%d-%d):" % (i + 3), (i + 13)) )
+        f.seek(i)
+
+        try:
+            byte = int( f.read(testLen) )
+
+        except:
+            print 'Something went wrong when reading index %d of pi.bin! Exiting program.' % i
+            exit(1)
+
+        while byte != '':
+            if userInput == byte:
+                i    += testLen
+                byte = int( f.read(testLen) )
             else:
-                userInput = int( raw_input("Enter all digits up to the feynman point: ")
-
-
-            if not userInput == byte:
-                print 'You recited %d digits of pi (Typed %d instead of %d)' % (i, int(userInput), int(byte))
+                print 'You remembered %d digits of pi! (Typed %d instead of %d)' % (i, userInput, byte)
                 break
 
-            i    += 1
-            byte = int( f.read(1) )
+            try:
+                if testLen in [1 , 2]:
+                    output = '%d: ' % (i + 1)
+
+                elif testLen == 10:
+                    output = '%d->%d: ' % i, (i + 10)
+
+                else:
+                    output = "Enter all digits up to the feynman point:"
+
+                userInput = int( raw_input(output) )
+
+            except:
+                print 'Something went wrong when obtaining user input! Exiting program.'
+                exit(1)
         else:
-            print 'You recited all the way to the Feynman point! Congratulations!'''
+            print 'You got to all the way to the Feynman point! Congratulations!'
 
 def main():
     while True:
@@ -32,6 +63,7 @@ def main():
             print 'For single digit reciting, type \'1\' or \'single\''
             print 'For ten digits at a time, type \'2\' or \'ten\''
             print 'For the all digits at once, type \'3\' or \'all\''
+            print 'To start reciting at a certain, type \'4\' or \'position\''
             print '\nAny bugs, issues, requests? Put them in the GitHub repo.\n'
 
         elif userInput.lower() == 'quit':
@@ -47,6 +79,10 @@ def main():
 
         elif userInput.lower() in ['3', 'all']:
             test(-1)
+            break
+
+        elif userInput.lower() in ['4', 'position']:
+            test(2)
             break
 
 if __name__ == '__main__':
